bf4def28db11d4bf31d5283711ec1abdefa8c7ca
Walber <walber17@gmail.com>
2023-01-16 21:52:34 -0300

    First Version

--- main.py
+++ main.py
@@ -20,104 +20,12 @@ from kivy.uix.widget import Widget
 import random
 from functools import partial
 import os
-# from questions import QUESTIONS
+from questions import QUESTIONS
 
 # Variables
 TOTAL_POINTS = 0
 TOTAL_QUESTIONS = 0
 os.environ['KIVY_ORIENTATION'] = 'Portrait'
-
-QUESTIONS = [
-
-    {
-        "text": "Qual o menor Átomo que exite?",
-        "options": [
-            "Hidrogênio",
-            "Oxigênio",
-            "Carbono",
-            "Sódio"
-        ],
-        "image": None
-    },
-
-    {
-        "text": "Qual o Átomo mais comum?",
-        "options": [
-            "Hidrogênio",
-            "Oxigênio",
-            "Carbono",
-            "Sódio"
-        ],
-        "image": None
-    },
-
-    {
-        "text": "Qual a ligação mais forte?",
-        "options": [
-            "Iônica",
-            "Covalente",
-            "Molecular",
-        ],
-        "image": None
-    },
-
-    {
-        "text": "Qual a Fórmula da Água?",
-        "options": [
-            "H_2 O",
-            "HO",
-            "H2CO3",
-            "NaCl"
-        ],
-        "image": None
-    },
-
-    {
-        "text": "Quantos Átomos existem na formula da água?",
-        "options": [
-            3,
-            4,
-            2,
-            1
-        ],
-        "image": None
-    },
-
-    {
-        "text": "Qual o Átomo mais importante para Química Orgânica?",
-        "options": [
-            "Carbono",
-            "Oxigênio",
-            "Hidrogênio",
-            "Sódio",
-        ],
-        "image": None
-    },
-
-    {
-        "text": "O grupo -OH ligado a um carbono tetraédrico é típico de qual grupo funcional?",
-        "options": [
-            "Álcool",
-            "Cetona",
-            "Ácido Carboxílico",
-            "Enol",
-        ],
-        "image": None
-    },
-
-    {
-        "text": "O que une as moléculas de água?",
-        "options": [
-            "Ligação de Hidrogênio",
-            "Vander Walls",
-            "Ligação Iônica",
-            "Ligação Metálica",
-        ],
-        "image": None
-    },
-
-]
-
 
 # Screens
 class MainScreen(Screen):
